60194e816251a97d6857b397a1c4548b8d7686e3
wicaksuu <wicaksono.bayuaji.wba@gmail.com>
2023-05-20 22:19:22 +0700

    dsa

--- engine/fuction/get_data.php
+++ engine/fuction/get_data.php
@@ -7,8 +7,8 @@ function get_data($start_from, $end_from = null, $rj_ri = 'rj', $surety_id = 2)
         $end_from = date('Y-m-d', time());
     }
     console_log('Get all data patient information');
-
-    $data = "sEcho=9&iColumns=10&sColumns=%2C%2C%2C%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=" . $config['length'] . "&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=false&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=false&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=false&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=false&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=false&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=false&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=false&mDataProp_7=7&sSearch_7=&bRegex_7=false&bSearchable_7=true&bSortable_7=false&mDataProp_8=8&sSearch_8=&bRegex_8=false&bSearchable_8=true&bSortable_8=false&mDataProp_9=9&sSearch_9=&bRegex_9=false&bSearchable_9=true&bSortable_9=false&sSearch=&bRegex=false&iSortCol_0=4&sSortDir_0=desc&iSortingCols=1&fil_jenis_waktu=periode&fil_tgl1=$start_from&fil_tgl2=$end_from&fil_jenis_layanan=$rj_ri&fil_tmp_layanan=0&fil_stt=0&fil_file_lap=1%7C%7C2%7C%7C3%7C%7C4%7C%7C5%7C%7C6%7C%7C7%7C%7C&surety_id=$surety_id&jns_tgl=0";
+    $preload = 100;
+    $data = "sEcho=9&iColumns=10&sColumns=%2C%2C%2C%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=" . $preload . "&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=false&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=false&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=false&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=false&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=false&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=false&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=false&mDataProp_7=7&sSearch_7=&bRegex_7=false&bSearchable_7=true&bSortable_7=false&mDataProp_8=8&sSearch_8=&bRegex_8=false&bSearchable_8=true&bSortable_8=false&mDataProp_9=9&sSearch_9=&bRegex_9=false&bSearchable_9=true&bSortable_9=false&sSearch=&bRegex=false&iSortCol_0=4&sSortDir_0=desc&iSortingCols=1&fil_jenis_waktu=periode&fil_tgl1=$start_from&fil_tgl2=$end_from&fil_jenis_layanan=$rj_ri&fil_tmp_layanan=0&fil_stt=0&fil_file_lap=1%7C%7C2%7C%7C3%7C%7C4%7C%7C5%7C%7C6%7C%7C7%7C%7C&surety_id=$surety_id&jns_tgl=0";
     $url  = $config['url'] . "/pelayanan_medis/generate_report_penjamin/get_data";
     $resp =  curlRequest($url, 'POST', $data, null);
 
@@ -25,6 +25,9 @@ function get_data($start_from, $end_from = null, $rj_ri = 'rj', $surety_id = 2)
                         mkdir($patch, 0777, true);
                     }
                     $seps[] = ["sep" => $sep, "dir" => $patch];
+                    if (count($seps) > $config['length']) {
+                        break;
+                    }
                 }
             }
         }
